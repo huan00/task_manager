af0d8da3fbc72ed6ddd27e86a9aec2ad5efd3e70
huan00 <96999144+huan00@users.noreply.github.com>
2022-07-05 16:38:22 -0400

    add delete button

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css'
 import { Typography, Fab } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
 import Tasks from '../components/Tasks'
+import NewTask from '../components/NewTask'
 
-const index = () => {
+const Index = () => {
+  const [addTaskModal, setAddTaskModal] = useState(false)
+  const [tasks, setTasks] = useState([
+    { urgent: 'on', task: 'select status to complete', status: 'incomplete' }
+  ])
+
+  const addTask = () => {
+    setAddTaskModal((addTaskModal) => !addTaskModal)
+  }
+
+  const storeTask = (task) => {
+    setTasks((tasks) => [...tasks, task])
+  }
+
+  const handleStatus = (idx, status) => {
+    const newTasks = tasks.map((task, index) => {
+      return index === idx ? { ...task, status: status } : task
+    })
+    setTasks(newTasks)
+  }
+  const handleUrgent = (idx, urg) => {
+    const newTasks = tasks.map((task, index) => {
+      return index === idx ? { ...task, urgent: urg } : task
+    })
+    setTasks(newTasks)
+  }
+
+  const handleTask = (idx, text) => {
+    const newTasks = tasks.map((task, index) => {
+      return index === idx ? { ...task, task: text } : task
+    })
+    setTasks(newTasks)
+  }
+
+  const handleDelete = (idx) => {
+    const newTasks = tasks.filter((task, index) => index !== idx)
+    setTasks(newTasks)
+  }
+
   return (
     <div>
       <div className="header">
@@ -12,6 +51,7 @@ const index = () => {
           All Tasks
         </Typography>
       </div>
+      <NewTask modal={addTaskModal} addTask={addTask} storeTask={storeTask} />
       <div className="taskContainer">
         <div className="taskLabel">
           <Typography variant="h6" component={''}>
@@ -23,11 +63,28 @@ const index = () => {
           <Typography variant="h6" component={''}>
             Status
           </Typography>
+          <Typography variant="h6" component={''}>
+            Delete
+          </Typography>
         </div>
-        <Tasks />
+        {tasks
+          .sort((taskA, taskB) => taskA.status > taskB.status)
+          .map((task, idx) => (
+            <Tasks
+              key={idx}
+              urgent={task.urgent}
+              task={task.task}
+              status={task.status}
+              idx={idx}
+              handleStatus={handleStatus}
+              handleUrgent={handleUrgent}
+              handleTask={handleTask}
+              handleDelete={handleDelete}
+            />
+          ))}
       </div>
       <div className="addTask">
-        <Fab size="large" color="primary" aria-label="add">
+        <Fab size="large" color="primary" aria-label="add" onClick={addTask}>
           <AddIcon />
         </Fab>
       </div>
@@ -35,4 +92,4 @@ const index = () => {
   )
 }
 
-export default index
+export default Index
